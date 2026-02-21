202fe0b902eada686f964f4d3849becd8279a130
NastyaPolyakova <pmanastasiyapm@mail.ru>
2026-02-21 15:28:27 +0300

    Add conditional operators and loops example

---
+++ 1_lesson/2.js
@@ -0,0 +1,23 @@
+//Условные операторы
+
+let time = 14;
+
+if (time < 12){
+  console.log("Good morning");
+}else if (time < 18){
+  console.log("Good afternoon")
+}else{
+  console.log("Good night")
+}
+
+//циклы
+
+for (let i = 0; i <= 5; i++){
+  console.log(i);
+}
+
+let count = 0;
+while(count < 20){
+  count++;
+  console.log(count);
+}
